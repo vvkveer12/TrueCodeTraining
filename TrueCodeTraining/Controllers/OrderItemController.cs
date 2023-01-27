4ce4f2ab54e7cdd1abb282d6024d5c5087cac5f6
Vivek008 <vivekyjnp2001@gmail.com>
2023-01-27 19:03:14 +0530

    Add Product

---
+++ TrueCodeTraining/Controllers/OrderItemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrueCode.Model;
+
+namespace TrueCodeTraining.Controllers
+{
+    public class OrderItemController : Controller
+    {
+        // GET: OrderItem
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
